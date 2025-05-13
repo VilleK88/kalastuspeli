59255527444073a9120adef408618e6a0af25ad8
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-05-13 12:22:13 +0300

    Reeling in the employer now works partially.

--- Assets/Scripts/PlayerScripts/Bait.cs
+++ Assets/Scripts/PlayerScripts/Bait.cs
@@ -1,6 +1,23 @@
+using Unity.Hierarchy;
 using UnityEngine;
 
 public class Bait : MonoBehaviour
 {
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Employer"))
+        {
+            Transform hips = collision.transform.root.Find("mixamorig:Hips");
+            if(hips != null)
+            {
+                Employer employer = hips.GetComponentInParent<Employer>();
+                if(employer != null)
+                {
+                    employer.transform.SetParent(transform);
+                    Animator anim = employer.GetComponent<Animator>();
+                    if (anim != null) anim.enabled = false;
+                }
+            }
+        }
+    }
 }
