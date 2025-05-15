39124f9cb4d70342fb4d273ef28de11a6bfd7ed9
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-05-15 07:10:39 +0300

    Clicking the mapbox tiles now works

---
+++ Assets/Scripts/UI/MapClickHandler.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
+using UnityEngine.EventSystems;
+using Unity.VisualScripting;
+
+public class MapClickHandler : MonoBehaviour, IPointerClickHandler
+{
+    public AbstractMap map;
+    public Transform player;
+    public Transform waypoint2;
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+        Ray ray = Camera.main.ScreenPointToRay(eventData.position);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            Vector2d latLon = map.WorldToGeoPosition(hit.point);
+            Vector3 newWorldPos = map.GeoToWorldPosition(latLon);
+            newWorldPos.y = waypoint2.position.y;
+
+            waypoint2.position = newWorldPos;
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Clicked via raycast");
+                Vector2d latLon = map.WorldToGeoPosition(hit.point);
+                Vector3 newWorldPos = map.GeoToWorldPosition(latLon);
+                newWorldPos.y = waypoint2.position.y;
+
+                waypoint2.position = newWorldPos;
+            }
+        }
+    }
+}
