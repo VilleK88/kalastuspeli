ec6a335664c648b2a8c4fbe0e4a9011045bbd3cd
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-05-20 13:36:00 +0300

    The spawn height of the EventPointers can now be set through the script

--- Assets/Scripts/Managers/EventPointer.cs
+++ Assets/Scripts/Managers/EventPointer.cs
@@ -8,6 +8,7 @@ public class EventPointer : MonoBehaviour
     [SerializeField] float rotationSpeed = 50f;
     [SerializeField] float amplitude = 2f;
     [SerializeField] float frequency = 0.5f;
+    float baseHeight = 8f;
 
     LocationStatus playerLocation;
     public Vector2d eventPos;
@@ -27,7 +28,9 @@ public class EventPointer : MonoBehaviour
     void FloatAndRotatePointer()
     {
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-        transform.position = new Vector3(transform.position.x, (Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude) + 15, transform.position.z);
+        //transform.position = new Vector3(transform.position.x, (Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude) + 15, transform.position.z);
+        float floatOffset = Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = new Vector3(transform.position.x, baseHeight + floatOffset, transform.position.z);
     }
 
     private void OnMouseDown()
