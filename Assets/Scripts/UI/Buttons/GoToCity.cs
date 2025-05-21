b631f238ffd6559e0c0988bca99c33fdb862f64a
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-05-21 15:36:24 +0300

    A subtle hover effect added to the buttons so that they slightly enlarge

--- Assets/Scripts/UI/Buttons/GoToCity.cs
+++ Assets/Scripts/UI/Buttons/GoToCity.cs
@@ -1,13 +1,47 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using System.Collections;
 
-public class GoToCity : MonoBehaviour
+public class GoToCity : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public City city;
+
+    Vector3 originalScale;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
 
     public void GoToThisCity()
     {
         GameManager.Instance.SetCity(city);
         SceneManager.LoadScene("3 - City");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StartCoroutine(ScaleTo(originalScale * 1.1f, 0.2f));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StartCoroutine(ScaleTo(originalScale, 0.2f));
+    }
+
+    IEnumerator ScaleTo(Vector3 target, float duration)
+    {
+        Vector3 start = transform.localScale;
+        float time = 0f;
+
+        while(time < duration)
+        {
+            transform.localScale = Vector3.Lerp(start, target, time / duration);
+            time += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        transform.localScale = target;
     }
 }
--- Assets/Scripts/UI/Buttons/GoToMap.cs
+++ Assets/Scripts/UI/Buttons/GoToMap.cs
@@ -1,10 +1,44 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class GoToMap : MonoBehaviour
+public class GoToMap : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    Vector3 originalScale;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
     public void GoBackToMap()
     {
         SceneManager.LoadScene("2 - Map");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StartCoroutine(ScaleTo(originalScale * 1.3f, 0.2f));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StartCoroutine(ScaleTo(originalScale, 0.2f));
+    }
+
+    IEnumerator ScaleTo(Vector3 target, float duration)
+    {
+        Vector3 start = transform.localScale;
+        float time = 0f;
+
+        while(time < duration)
+        {
+            transform.localScale = Vector3.Lerp(start, target, time / duration);
+            time += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        transform.localScale = target;
     }
 }
