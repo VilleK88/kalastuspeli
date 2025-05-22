4ae619bbf6216991e19dfc61e3b79b51f80e99ee
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-05-22 16:48:08 +0300

    Menu in progress...
    Audio sliders added and functionalities coded for them.

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] GameObject settingsBG;
+    [Header("Audio")]
+    [SerializeField] GameObject audioSettings;
+    public Slider masterVol, musicVol, sfxVol;
+    public AudioMixer MainAudioMixer;
+
+    private void Start()
+    {
+        masterVol.value = PlayerPrefs.GetFloat("MasterVolume", masterVol.value);
+        musicVol.value = PlayerPrefs.GetFloat("MusicVolume", musicVol.value);
+        sfxVol.value = PlayerPrefs.GetFloat("SFXVolume", sfxVol.value);
+        ChangeMasterVolume();
+        ChangeMusicVolume();
+        ChangeSFXVolume();
+        masterVol.onValueChanged.AddListener(delegate { ChangeMasterVolume(); });
+        musicVol.onValueChanged.AddListener(delegate { ChangeMusicVolume(); });
+        sfxVol.onValueChanged.AddListener(delegate { ChangeSFXVolume(); });
+    }
 
     public void OpenSettingsMenu()
     {
@@ -13,5 +31,34 @@ public class MenuManager : MonoBehaviour
     public void CloseSettingsMenu()
     {
         settingsBG.SetActive(false);
+    }
+
+    public void OpenAudioSettings()
+    {
+        audioSettings.SetActive(true);
+    }
+
+    public void CloseAudioSettings()
+    {
+        audioSettings.SetActive(false);
+    }
+
+    public void ChangeMasterVolume()
+    {
+        MainAudioMixer.SetFloat("MasterVol", masterVol.value);
+        PlayerPrefs.SetFloat("MasterVolume", masterVol.value);
+        PlayerPrefs.Save();
+    }
+    public void ChangeMusicVolume()
+    {
+        MainAudioMixer.SetFloat("MusicVol", musicVol.value);
+        PlayerPrefs.SetFloat("MusicVolume", musicVol.value);
+        PlayerPrefs.Save();
+    }
+    public void ChangeSFXVolume()
+    {
+        MainAudioMixer.SetFloat("SFXVol", sfxVol.value);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVol.value);
+        PlayerPrefs.Save();
     }
 }
