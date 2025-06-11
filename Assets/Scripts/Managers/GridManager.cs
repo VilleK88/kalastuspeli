e4b66f5824d24729e5e98440cc2e499aa305c1c3
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-06-11 16:23:40 +0300

    Grid/Marker system in progress...

--- Assets/Scripts/Managers/GridManager.cs
+++ Assets/Scripts/Managers/GridManager.cs
@@ -27,17 +27,52 @@ public class GridManager : MonoBehaviour
     int addition = 100;
     int gridHeight = 8;
 
+    Vector3 originPosition;
+
     private void Start()
     {
-        startingValueX = transformStartX;
+        //transform.position = GetCenterOfChildren(CityNavMeshSurfaceBuilder.Instance.transform);
+        originPosition = GetCenterOfChildren(CityNavMeshSurfaceBuilder.Instance.transform);
+        //startingValueX = transformStartX;
         InitializeGrid();
+    }
+
+    Vector3 GetCenterOfChildren(Transform parent)
+    {
+        Vector3 total = Vector3.zero;
+        int count = 0;
+
+        foreach(Transform child in parent)
+        {
+            total += child.position;
+            count++;
+        }
+
+        return total / count;
     }
 
     void InitializeGrid()
     {
-        for(int i = 0; i < gridAmount; i++)
+        /*for(int i = 0; i < gridAmount; i++)
         {
             InitializeGridRow();
+        }*/
+
+        float halfGridSize = (gridAmount - 1) * addition / 2;
+
+        for(int z = 0; z < gridAmount; z++)
+        {
+            for(int x = 0; x < gridAmount; x++)
+            {
+                float offsetX = -halfGridSize + x * addition;
+                float offsetZ = -halfGridSize + z * addition;
+
+                Vector3 currentPosition = new Vector3(originPosition.x + offsetX, gridHeight, originPosition.z + offsetZ);
+
+                GameObject gridPrefabInstance = Instantiate(gridPrefab, currentPosition, Quaternion.identity);
+                grid.Add(gridPrefabInstance);
+                gridPrefabInstance.transform.parent = parentObject.transform;
+            }
         }
     }
 
